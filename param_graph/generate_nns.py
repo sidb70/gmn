11a2895821d9a6ef7f926492a0d79a5f3cdaa722
uzairname <23709618+uzairname@users.noreply.github.com>
2024-07-06 20:05:00 -0400

    generate random cnn

---
+++ param_graph/generate_nns.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+
+
+def generate_random_cnn(in_dim=32, in_channels=3, out_dim=10) -> nn.Module:
+  """
+  creates a cnn with a random number of convolutional layers followed by random linear layers
+  assumes input to the cnn is (batch size, in_channels, in_dim, in_dim) shape
+
+  Args:
+  - in_dim: int, the height and width of the input image
+  - in_channels: int, the number of channels in the input image
+  - out_dim: int, the number of classes
+
+  Returns:
+  - model: nn.Module, the randomly generated CNN model
+
+  """
+
+  layers = [] # list of tuples (layer, out_shape)
+
+  while True:
+    if len(layers) == 0:
+      out_channels = 2**torch.randint(4, 8, (1,)).item()
+      layers.append((nn.Conv2d(in_channels, out_channels, 3), (out_channels, in_dim-2, in_dim-2)))
+      
+    elif isinstance(layers[-1][0], nn.Conv2d):
+      # batch norm layer
+      n_features = layers[-1][0].out_channels
+      layers.append((nn.BatchNorm2d(n_features), layers[-1][1]))
+      layers.append((nn.ReLU(), layers[-1][1]))
+
+      # randomly either add another conv layer or switch to linear layers
+      if torch.rand(1).item() < 0.7 or \
+        layers[-1][1][1] < 3 or layers[-1][1][2] < 3: # check if out shape is smaller than the kernel size
+
+        out_channels = 2**torch.randint(4, 8, (1,)).item()
+        layers.append((nn.Conv2d(layers[-1][1][0], out_channels, 3), (out_channels, layers[-1][1][1]-2, layers[-1][1][2]-2)))
+      else:
+        # begin linear layers
+        layers.append((nn.Flatten(), (layers[-1][1][0]*layers[-1][1][1]*layers[-1][1][2],)))
+        continue
+
+    elif isinstance(layers[-1][0], nn.Flatten) or isinstance(layers[-1][0], nn.ReLU):
+      # linear layer
+      if torch.rand(1).item() < 0.5:
+        out_features = 2**torch.randint(4, 8, (1,)).item()
+        layers.append((nn.Linear(layers[-1][1][0], out_features), (out_features,)))
+        layers.append((nn.ReLU(), layers[-1][1]))
+      else:
+        layers.append((nn.Linear(layers[-1][1][0], out_dim), (out_dim,)))
+        break
+
+  print([layer[1] for layer in layers])
+  return nn.Sequential(*[layer[0] for layer in layers])
