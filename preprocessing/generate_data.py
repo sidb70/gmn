dcb0c5e4c6ca8064d2e8157fc8318149ca4a4dc8
Siddhartha Bhattacharya <bhatta70@msu.edu>
2024-07-22 19:57:59 -0400

    write and load cifar 10 with ffcv

--- preprocessing/generate_data.py
+++ preprocessing/generate_data.py
@@ -3,27 +3,30 @@ import sys
 import torch
 import torch.nn as nn
 from torch import optim
-from torch.utils.data.sampler import SubsetRandomSampler
-from torch.utils.data import DataLoader
-
-from torchvision import transforms
-from torchvision.datasets import CIFAR10
-
+import torchvision
+import numpy as np
+#from torch.utils.data import DataLoader
+from ffcv.loader import Loader, OrderOption
+from ffcv.transforms import ToTensor, ToDevice,  RandomHorizontalFlip, RandomTranslate, Cutout, ToTorchImage, Convert, Squeeze
+from ffcv.fields.decoders import IntDecoder, SimpleRGBImageDecoder
+from ffcv.fields.basics import Operation
+from typing import List
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from preprocessing.generate_nns import generate_random_cnn
 from param_graph.gmn_lim.model_arch_graph import seq_to_feats
 
 
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 def train_random_cnns(
     n_architectures=10,
     train_size=None,
-    batch_size=4,
+    batch_size=512,
     n_epochs=2,
     lr=0.001, 
     momentum=0.9,
-    directory='data/cnn',
+    num_workers=4,
+    directory='data/',
     replace_if_existing=False,
     **random_cnn_kwargs
 ):
@@ -42,28 +45,34 @@ def train_random_cnns(
 
     torch.manual_seed(0)
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    ###
+    #https://docs.ffcv.io/ffcv_examples/cifar10.html
+    CIFAR_MEAN = [125.307, 122.961, 113.8575]
+    CIFAR_STD = [51.5865, 50.847, 51.255]
+    loaders = {}
+    for name in ['train', 'test']:
+        label_pipeline: List[Operation] = [IntDecoder(), ToTensor(), ToDevice('cuda:0'), Squeeze()]
+        image_pipeline: List[Operation] = [SimpleRGBImageDecoder()]
 
-    trainset = CIFAR10(root='./data/cifar10', train=True, download=True, transform=transform)
-    if train_size is None:
-        train_sampler = None
-    else:
-        train_sampler = SubsetRandomSampler(torch.randperm(len(trainset))[:train_size])
-    
-    trainloader = DataLoader(trainset, batch_size=batch_size, sampler=train_sampler)
+        image_pipeline.extend([
+            ToTensor(),
+            ToDevice(DEVICE, non_blocking=True),
+            ToTorchImage(),
+            Convert(torch.float32),
+            torchvision.transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+        ])
 
-    testset = CIFAR10(root='./data/cifar10', train=False, download=True, transform=transform)
-    if train_size is None:
-        test_sampler = None
-    else:
-        test_size = train_size // 4
-        test_sampler = SubsetRandomSampler(torch.randperm(len(testset))[:test_size])
-    
-    testloader = DataLoader(testset, batch_size=batch_size, sampler=test_sampler)
-
+        # Create loaders
+        loaders[name] = Loader(os.path.join(directory,'cifar10', f'cifar_{name}.beton'),
+                                batch_size=batch_size,
+                                num_workers=num_workers,
+                                order=OrderOption.RANDOM,
+                                drop_last=(name == 'train'),
+                                pipelines={'image': image_pipeline,
+                                        'label': label_pipeline})
+    ###
+    trainloader = loaders['train']
+    testloader = loaders['test']
     features = []
     accuracies = []
 
@@ -118,24 +127,23 @@ def train_random_cnns(
     print('saving data')
 
     # ensure that the directory is valid and exists
-    os.makedirs(directory, exist_ok=True)
+    save_dir = os.path.join(directory, 'cnn')
+    os.makedirs(save_dir, exist_ok=True)
 
     if replace_if_existing:
-        features_path = os.path.join(directory, 'features.pt')
-        accuracies_path = os.path.join(directory, 'accuracies.pt')
+        features_path = os.path.join(save_dir, 'features.pt')
+        accuracies_path = os.path.join(save_dir, 'accuracies.pt')
         if os.path.exists(features_path):
             os.remove(features_path)
         if os.path.exists(accuracies_path):
             os.remove(accuracies_path)
 
     else:
-        if os.path.exists(os.path.join(directory, 'features.pt')) and os.path.exists(os.path.join(directory, 'accuracies.pt')):
+        if os.path.exists(os.path.join(save_dir, 'features.pt')) and os.path.exists(os.path.join(save_dir, 'accuracies.pt')):
             # load
-            features = torch.load(os.path.join(directory, 'features.pt')) + features
-            accuracies = torch.load(os.path.join(directory, 'accuracies.pt')) + accuracies
+            features = torch.load(os.path.join(save_dir, 'features.pt')) + features
+            accuracies = torch.load(os.path.join(save_dir, 'accuracies.pt')) + accuracies
 
-    torch.save(features, os.path.join(directory, 'features.pt'))
-    torch.save(accuracies, os.path.join(directory, 'accuracies.pt'))
+    torch.save(features, os.path.join(save_dir, 'features.pt'))
+    torch.save(accuracies, os.path.join(save_dir, 'accuracies.pt'))
 
-if __name__ == '__main__':
-    train_random_cnns(n_architectures=10, train_size=1000, n_epochs=2, replace_if_existing=True, directory='data/cnn')
