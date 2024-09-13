7661f895a57fdbf53d496ebb0c95411202bba0de
SB <bhatta70@msu.edu>
2024-09-13 18:02:36 -0400

    fix running loss

--- generate_train_data.py
+++ generate_train_data.py
@@ -3,4 +3,4 @@ from preprocessing.hpo_configs import RandHyperparamsConfig, RandHyperparamsConf
 
 if __name__ == '__main__':
     config = RandHyperparamsConfig(n_epochs_range = [50,150])
-    train_random_cnns_hyperparams('data/hpo', n_architectures=30000, random_hyperparam_config=config)
+    train_random_cnns_hyperparams('data/hpo', n_architectures=30000, random_hyperparams_config=config)
